a4c3a1fee1f745880832f8e51035d55204bd79ef
Brayton-Lordianto <braylord30@gmail.com>
2023-03-31 20:55:16 -0400

    xcodeproj

---
+++ HackPrinceton/HackPrinceton/HackPrincetonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackPrincetonApp.swift
+//  HackPrinceton
+//
+//  Created by Brayton Lordianto on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct HackPrincetonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
