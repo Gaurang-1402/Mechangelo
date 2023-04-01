0ec22f06d7987a40ac9da4aa3454b26b80f6e507
Gaurang Ruparelia <gr2159@nyu.edu>
2023-04-01 18:01:45 -0400

    Add image_to_contours function # This commit adds the image_to_contours function, which takes an image and converts

--- gcode/gcode-conversion.py
+++ gcode/gcode-conversion.py
@@ -30,3 +30,40 @@ def process_gcode(gcode):
     processed_chunks = [process_chunk(chunk) for chunk in non_empty_chunks]
     
     return processed_chunks
+
+
+@memoized
+def image_to_contours(image):
+    # Set the path to the image-to-gcode directory
+    image_to_gcode_dir = '/Users/ryan/CleanCode/Github/image-to-gcode'
+    
+    # Check if the input is a valid image or a string (file path)
+    assert is_image(image) or isinstance(image, str)
+    
+    # If the input is a string (file path), load the image and call the function again
+    if isinstance(image, str):
+        image = load_image(image, use_cache=False)
+        return image_to_contours(image)
+    
+    # Resize the image to fit within the specified width
+    resized_image = resize_image_to_fit(image, width=150)
+    
+    # Change the current directory temporarily to the image-to-gcode directory
+    with SetCurrentDirectoryTemporarily(image_to_gcode_dir):
+        input_file = 'input.png'
+        output_file = 'out.gcode'
+        
+        # Save the resized image as input_file
+        save_image(resized_image, input_file)
+        
+        # Run the image-to-gcode script using the input_file and save the result as output_file
+        os.system('python3 image_to_gcode.py -i %s -o %s' % (input_file, output_file))
+        
+        # Read the contents of the output_file as a string
+        gcode = text_file_to_string(output_file)
+    
+    # Process the gcode and obtain the contours
+    contours = process_gcode(gcode)
+    
+    # Normalize the contours and return the result
+    return normalize_contours(contours)
